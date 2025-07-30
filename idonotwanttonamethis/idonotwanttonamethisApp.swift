d1231bdc32d42ab9a740498aaa51a1d901fc5b5d
violet macgilvray <shannonmacgilvray@gmail.com>
2025-07-30 10:20:43 -0700

    Initial Commit

---
+++ idonotwanttonamethis/idonotwanttonamethisApp.swift
@@ -0,0 +1,17 @@
+//
+//  idonotwanttonamethisApp.swift
+//  idonotwanttonamethis
+//
+//  Created by Scholar on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct idonotwanttonamethisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
