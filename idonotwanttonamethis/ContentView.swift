35ee504584d66236e34697ab709d0a93448ebf31
violet macgilvray <shannonmacgilvray@gmail.com>
2025-07-31 09:36:22 -0700

    o

--- idonotwanttonamethis/ContentView.swift
+++ idonotwanttonamethis/ContentView.swift
@@ -8,16 +8,201 @@
 import SwiftUI
 
 struct ContentView: View {
+  //  @State private var buttonColor: Color = .blue // Initial color
+    @State private var colors: [Color] = [.black, .red, .green, .blue, .orange, .purple, .yellow] // Array of colors
+       @State private var currentColorIndex: Int = 0 // Tracks the current color index
+   
+    @State private var currentColorIndex2: Int = 0 // Tracks the current color index
+    @State private var colors2: [Color] = [.black, .red, .green, .blue, .orange, .purple, .yellow] // Array of colors
+    
+    @State private var currentColorIndex3: Int = 0 // Tracks the current color index
+    @State private var colors3: [Color] = [.black, .red, .green, .blue, .orange, .purple, .yellow] // Array of colors
+    
+    @State private var currentColorIndex4: Int = 0 // Tracks the current color index
+    @State private var colors4: [Color] = [.black, .red, .green, .blue, .orange, .purple, .yellow] // Array of colors
+    
+    @State private var currentColorIndex5: Int = 0 // Tracks the current color index
+    @State private var colors5: [Color] = [.black, .red, .green, .blue, .orange, .purple, .yellow] // Array of colors
+    
+    @State private var currentColorIndex6: Int = 0 // Tracks the current color index
+    @State private var colors6: [Color] = [.black, .red, .green, .blue, .orange, .purple, .yellow] // Array of colors
+    
+    @State private var currentColorIndex7: Int = 0 // Tracks the current color index
+    @State private var colors7: [Color] = [.black, .red, .green, .blue, .orange, .purple, .yellow ] // Array of colors
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+       VStack {
+           Button(" monday                         ") {
+               if currentColorIndex == 6  {
+                   currentColorIndex = 0
+            
+               }
+               else {
+                   currentColorIndex += 1
+               }
+      
+           }
+           .font(.title2)
+           .frame(width: 500.0, height: 100.0) // Sets a fixed width
+           .buttonStyle(.borderedProminent)
+           .tint(colors[currentColorIndex])
+           
+           Button  (" tuesday                         ") {
+               if currentColorIndex2 == 6  {
+                   currentColorIndex2 = 0
+            
+               }
+               else {
+                   currentColorIndex2 += 1
+               }
+      
+           }
+           .font(.title2)
+           .frame(width: 500.0, height: 100.0) // Sets a fixed width
+           .buttonStyle(.borderedProminent)
+           .tint(colors2[currentColorIndex2])
+           
+           Button  (" wednesday                   ") {
+               if currentColorIndex3 == 6  {
+                   currentColorIndex3 = 0
+            
+               }
+               else {
+                   currentColorIndex3 += 1
+               }
+      
+           }
+           .font(.title2)
+           .frame(width: 500.0, height: 100.0) // Sets a fixed width
+           .buttonStyle(.borderedProminent)
+           .tint(colors3[currentColorIndex3])
+           
+           Button  (" thursday                        ") {
+               if currentColorIndex4 == 6  {
+                   currentColorIndex4 = 0
+            
+               }
+               else {
+                   currentColorIndex4 += 1
+               }
+      
+           }
+           .font(.title2)
+           .frame(width: 500.0, height: 100.0) // Sets a fixed width
+           .buttonStyle(.borderedProminent)
+           .tint(colors4[currentColorIndex4])
+           
+           Button  (" friday                              ") {
+               if currentColorIndex5 == 6  {
+                   currentColorIndex5 = 0
+            
+               }
+               else {
+                   currentColorIndex5 += 1
+               }
+      
+           }
+           .font(.title2)
+           .frame(width: 500.0, height: 100.0) // Sets a fixed width
+           .buttonStyle(.borderedProminent)
+           .tint(colors5[currentColorIndex5])
+           
+           Button  (" saturday                         ") {
+               if currentColorIndex6 == 6  {
+                   currentColorIndex6 = 0
+            
+               }
+               else {
+                   currentColorIndex6 += 1
+               }
+      
+           }
+           .font(.title2)
+           .frame(width: 500.0, height: 100.0) // Sets a fixed width
+           .buttonStyle(.borderedProminent)
+           .tint(colors6[currentColorIndex6])
+           
+           Button  (" sunday                           ") {
+               if currentColorIndex7 == 6  {
+                   currentColorIndex7 = 0
+            
+               }
+               else {
+                   currentColorIndex7 += 1
+               }
+      
+           }
+           .font(.title2)
+           .frame(width: 500.0, height: 100.0) // Sets a fixed width
+           .buttonStyle(.borderedProminent)
+           .tint(colors7[currentColorIndex7])
+           Text("mood tracker placeholder UI, just click until you hit")
+                Text("a color that fits your mood for that day.")
+      //     Button("Click Me") {
+               // Change the color when the button is tapped
+          //     buttonColor = .red
+              // color = isPressed ? .red : .blue
+             //  var body: some View {
+                   //   Button(action: {
+                          // Increment the index and cycle back to the beginning if it exceeds the array size
+                //          self.currentColorIndex = (self.currentColorIndex + 1) % self.colors.count
+             //         }) {
+                       //   Text("Click Me!")
+                        //      //.font(.title)
+                         //     .padding()
+                           //   .foreground//Color(.white)
+                      }
+                    //  .background(colors[currentColorIndex]) // Set background color based on the current index
+                      //.cornerRadius(10)
+                  }
+              }
+//VStack {
+  //  Grid {
+       // GridRow {
+        //    Text("R1, C1")
+          //  Text("R1, C2")
+            //Text("R1, C3")
+        //}
+        //GridRow {
+          //  Text("R2, C1")
+            //Text("R2, C2")
+              //  .gridCellColumns(2) // Spans two columns
+        //}
+        //GridRow {
+          //  Text("R3, C1")
+            //Text("R3, C2")
+           // Text("R3, C3")
+        //}
+   // }
+//}
+       //    Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+               /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+               
+               
+               // Button(action: { ... }): The action closure
+       //        Text("Click Me!") //.
+             //      .background(colors[currentColorIndex]):
+               //    .cornerRadius(10):
+               
+           //}
+
+        
+
+        
+         
+
+      //     }
+        //   .background(buttonColor)
+          // .foregroundColor(.white) // For text color
+         //  .cornerRadius(10) // Optional: for rounded corners
+           
+          //  Image(systemName: "globe")
+           //     .imageScale(.large)
+           //     .foregroundStyle(.tint)
+           // Text("Hello, world!")
+//        }
+      //  .padding()
+    
+
 
 #Preview {
     ContentView()
---
+++ idonotwanttonamethis/cdefsvhn.swift
@@ -0,0 +1,8 @@
+//
+//  cdefsvhn.swift
+//  idonotwanttonamethis
+//
+//  Created by Scholar on 7/30/25.
+//
+
+import Foundation
