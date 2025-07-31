ccfec6c0ee93102d4a00186476058a4ea8b96fd6
violet macgilvray <shannonmacgilvray@gmail.com>
2025-07-31 13:35:49 -0700

    t

---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  idonotwanttonamethis
+//
+//  Created by Scholar on 7/31/25.
+//
+
+import Foundation
---
+++ idonotwanttonamethis/qsdftrf.swift
@@ -0,0 +1,18 @@
+//
+//  qsdftrf.swift
+//  idonotwanttonamethis
+//
+//  Created by Scholar on 7/31/25.
+//
+
+import SwiftUI
+
+struct qsdftrf: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    qsdftrf()
+}
